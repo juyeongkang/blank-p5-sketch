c5fb7988e770ca1accd76e56d2f1c0ced2225e0b
Juyeong <landsofg@gmail.com>
2020-09-28 23:41:57 -0700

    randomizer1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,43 @@
-function setup() {
-  createCanvas(400, 400);
+// The things what i love
+let things = [{
+    who: "Father",
+    color: "special treat color"
+    },{
+    who: "Mother ",
+    color: "chartrese"
+    },{
+    who:"Brother",
+    color: "poop"
+    },{
+    who:"Girlfriend",
+    color: "blue"
+    },{
+    who:"Bestfriends",
+    color: "black"
+    },{
+    who:"Games",
+    color: "red"
+    },{
+    who:"Art",
+    color: "white"
+    }];
+
+   let randomIndex;
+
+    function setup() {
+      createCanvas(400, 400);
+      background(200);
+
 }
 
-function draw() {
-  background(120);
+    function draw() {
+
+    }
+
+   function mousePressed()  {
+     background(random(200, 255));
+     randomIndex = int(random(things.length));
+     text(things[randomIndex].who, 155, 155);
+     things.splice(randomIndex, 1);
+
 }
